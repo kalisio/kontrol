70e9cd91147311cd5fab4c04f4fcaecc6296bf1f
Luc Claustres <luc.claustres@orange.fr>
2020-12-11 20:02:35 +0100

    Updated default config

--- config.js
+++ config.js
@@ -1,29 +1,28 @@
 module.exports = {
-	docker: {},
-	jobs: {
-		kontrol: {
-			cron: '*/3 * * * * *',
-			url: 'http://localhost:8080/healthcheck',
-			method: 'GET',
-			headers: {},
-			searchParams: {},
-			retry: 1,
-			timeout: 10000,
-			notify: (error) => {
-				return {
-				   attachments: [{
-            color: 'danger',
-            mrkdwn_in: ['text'],
-            text: `*kontrol healthcheck failed*\n${error}\nNow healing by restarting`
-          }]
-				}
-			},
-			heal: async (docker, _) => {
-				const containers = await docker.listContainers()
-				let container = _.find(containers, (container => container.Image === 'kalisio/kontrol'))
-				container = await docker.getContainer(container.Id)
-				await container.restart()
-			}
-		}
-	}
+  docker: {},
+  jobs: {
+    kontrol: {
+      cron: '*/3 * * * * *',
+      url: 'http://localhost:8080/healthcheck',
+      method: 'GET',
+      headers: {},
+      searchParams: {},
+      retry: 1,
+      timeout: 10000,
+      notify: (error) => ({
+         attachments: [{
+          color: (error ? 'danger' : 'good'),
+          mrkdwn_in: ['text'],
+          text: (error ? `*kontrol healthcheck failed*\n${error}\nNow healing by restarting`
+                       : `*kontrol healthcheck succeeded*`)
+        }]
+      }),
+      heal: async (docker, _) => {
+        const containers = await docker.listContainers()
+        let container = _.find(containers, (container => container.Image === 'kalisio/kontrol'))
+        container = await docker.getContainer(container.Id)
+        await container.restart()
+      }
+    }
+  }
 }
